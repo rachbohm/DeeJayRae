d03ba64aeb057d7442a0ee360f167e5ba9a73329
Rachel <rachbohm@gmail.com>
2022-11-11 00:03:26 -0600

    started get-details-song-id route

--- backend/db/models/song.js
+++ backend/db/models/song.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Song.belongsTo(
         models.User,
-        { foreignKey: 'userId' }
+        { foreignKey: 'userId', as: "Artist" }
       );
       Song.belongsTo(
         models.Album,
--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -5,6 +5,26 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
+router.get('/:songId', async (req, res, next) => {
+  const songId = req.params.songId;
+  const song = await Song.findByPk(songId, {
+    include: [
+      {
+        model: User,
+        as: 'Artist'
+      },
+      {
+        model: Album
+      }
+    ]
+  });
+  if (!song) {
+    res.status(404);
+    return res.json({ message: "Song couldn't be found"});
+  }
+  return res.json(song)
+})
+
 router.get('/current', requireAuth, async (req, res, next) => {
   const { user } = req;
   const songs = await Song.findAll({
