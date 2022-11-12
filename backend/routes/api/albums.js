90523aa748d214810376bfa71ac2c900e6488cff
Rachel <rachbohm@gmail.com>
2022-11-12 13:13:06 -0600

    get all albums of current user route complete

--- backend/routes/api/albums.js
+++ backend/routes/api/albums.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User, Album } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
+router.get('/current', requireAuth, async (req, res, next) => {
+  const { user } = req;
+  const userAlbums = await Album.findAll({
+    where: {
+      userId: user.id
+    }
+  });
+  return res.json({Albums: userAlbums})
+});
 
 router.get('/', async (req, res) => {
   const allAlbums = await Album.findAll();
